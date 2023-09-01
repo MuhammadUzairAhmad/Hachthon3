aed5a2a6247530ddd6eeda915090950bee5759e9
muhammaduzair321 <135225948+MuhammadUzairAhmad@users.noreply.github.com>
2023-09-01 14:04:41 +0500

    first commit

---
+++ src/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from './models/UserModel';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    //   ignoreExpiration: false,
+      // secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: "process.env.JWT_SECRET",
+    });
+  }
+  async validate(payload: any) {
+    const { id } = payload;
+    console.log(id)
+    const user = await this.UserModel.findById(id);
+    if (!user) {
+      throw new UnauthorizedException('Login first to access this page');
+    }
+    console.log('strategy', user);
+    return user;
+  }
+}
